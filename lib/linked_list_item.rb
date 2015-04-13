7cedd4f553a733067201f3fe29a8b8839e8506b7
Brennan Willingham <brennanw@Brennan-Willinghams-MacBook-Pro.local>
2015-04-13 18:54:35 -0500

    Finished the first four tests

--- lib/linked_list_item.rb
+++ lib/linked_list_item.rb
@@ -1,3 +1,25 @@
 class LinkedListItem
   include Comparable
+  attr_reader :payload, :next_item
+
+  def initialize(payload)
+    @payload = payload
+  end
+
+  def next_item=(the_next_item)
+    raise ArgumentError if the_next_item == self
+    @next_item = the_next_item
+  end
+  def last?
+    if @next_item.nil?
+      return true
+    else
+      return false
+    end
+  end
+  def <=>(other)
+    if self.payload != other.payload
+      self.payload <=> other.payload
+    end
+  end
 end
