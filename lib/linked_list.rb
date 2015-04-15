33d24dabd042c9886176c89d8212fec4b826f473
Brennan Willingham <brennanw@Brennan-Willinghams-MacBook-Pro.local>
2015-04-15 15:36:51 -0500

    made a lot of methods and solved ll.rb

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,4 +1,87 @@
-class LinkedList
-  attr_reader :payload
+require_relative 'linked_list_item'
 
+class LinkedList
+
+  def initialize(*payload)
+    if payload[0]
+      lli = LinkedListItem.new(payload[0])
+      @curr  = lli
+      @first = lli
+      @indexCounter = 0
+      number = @indexCounter + 1
+      while payload[number]
+        llin = LinkedListItem.new(payload[number])
+        @curr.next_item = llin
+        @curr = @curr.next_item
+        number += 1
+        @indexCounter += 1
+      end
+    else
+      @curr = nil
+      @indexCounter = -1
+    end
+  end
+
+  def push(payload)
+    @indexCounter += 1
+    lli = LinkedListItem.new(payload)
+    if @curr != nil
+      @curr.next_item = lli
+    else
+      @first = lli
+    end
+    @curr = lli
+  end
+
+  def getPayload(index)
+    payloadLocation = @first
+    index.times do |number|
+      nextItem = payloadLocation.next_item
+      payloadLocation = nextItem
+    end
+    return payloadLocation
+  end
+
+  def get(index)
+    if index < 0 || index > @indexCounter
+      raise IndexError
+    elsif index == @indexCounter
+      return @curr.payload
+    else
+      return getPayload(index).payload
+    end
+  end
+
+  def size
+    @indexCounter += 1
+    return @indexCounter
+  end
+
+  def last
+    if @curr
+      return @curr.payload
+    else
+      @curr
+    end
+  end
+
+  def to_s
+    if @curr
+      iteration = 0
+      str       = ""
+      itemCount = @first
+      while iteration <= @indexCounter
+        if iteration == @indexCounter
+          str << itemCount.payload.to_s
+        else
+          str << itemCount.payload.to_s + ", "
+          itemCount = itemCount.next_item
+        end
+        iteration += 1
+      end
+      return "| #{str} |"
+    else
+      return "| |"
+    end
+  end
 end
--- lib/linked_list_item.rb
+++ lib/linked_list_item.rb
@@ -1,44 +1,29 @@
 class LinkedListItem
   include Comparable
-  attr_reader :payload, :next_item
+  attr_reader   :payload, :next_item
 
   def initialize(payload)
     @payload = payload
   end
 
-  def <=>(other)
-    type0      = Fixnum
-    type1      = String
-    type2      = Symbol
-    class1     = self.payload.class
-    class2     = other.payload.class
-
-    if (class1 == type0 && class2 == type0) || (class1 == type1 && class2 == type1) || (class1 == type2 && class2 == type2)
-      return self.payload <=> other.payload
-    elsif class1 == type0 && (class2 == type1 || class2 == type2)
-      return false
-    elsif class1 == type1 && class2 == type2
-      return false
+  def <=>(lli)
+    if self.payload.class == lli.payload.class
+      self.payload <=> lli.payload
     else
-      return true
+      self.payload.class.to_s <=> lli.payload.class.to_s
     end
   end
 
-  def next_item=(other)
-    raise ArgumentError if other == self
-    @next_item = other
+  def next_item=(lli)
+    raise ArgumentError, "Can't set next_item to self" if self == lli
+    @next_item = lli
   end
 
   def last?
-    if @next_item.nil?
-      return true
-    else
-      return false
-    end
+   @next_item.nil?
   end
 
-  def ===(other)
-    self.equal? other
+  def ===(lli)
+    self.equal? lli
   end
-
 end
